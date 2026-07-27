c2ce24ed080f5805bf58d6888c80233b9a6f4bb3
Valentin Palamara <valentin.palamara@hotmail.com>
2026-07-27 16:29:42 +0200

    mise en place d'un switch et correction de bug

--- retard.ps1
+++ retard.ps1
@@ -4,15 +4,32 @@ Toute ressemblance avec des situations ayant existées est purement fortuite.
 #>
 
 $retard = read-host -prompt "Minutes de retard "
-
 $financement = read-host -prompt "Etes vous finance par la region ? [oui/non]"
 
-if ($financement -match "oui") {
+<# Structure IF de première intention. 
+Ne prennait pas en compte les réponses différentes et "o" ou "n"
+
+if ($financement -match "oui" -or "") {
     write-host "t'es finance, de quoi tu te plains ?"
 }
 else {
     $indiv_price = [int]$retard * 0.13
     write-host " tu as perdu $indiv_price euros"
 }
+#>
 
+Switch -exact ($financement) {
+    "oui"   {write-host "t'es finance, de quoi tu te plains ?"
+            break }
+    "o"     {write-host "t'es finance, de quoi tu te plains ?"
+            break }
+    "non"   {$indiv_price = [int]$retard * 0.13
+            write-host " tu as perdu $indiv_price euros"
+            break }
+    "n"     {$indiv_price = [int]$retard * 0.13
+            write-host " tu as perdu $indiv_price euros"
+            break }
+    Default {write-host "la reponse n'est pas valide"
+            break }
+    }
 read-host
