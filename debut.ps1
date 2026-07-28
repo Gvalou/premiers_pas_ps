319a2aecd3eded04e1a1155748252d9c30cbe558
Valentin Palamara <valentin.palamara@hotmail.com>
2026-07-28 11:49:59 +0200

    debut des tp

---
+++ debut.ps1
@@ -0,0 +1,11 @@
+<#
+.DESCRIPTION   
+Voici la description du script
+#>
+
+write-host "Version de PS : " $PSVersionTable.PSVersion
+write-host "$(Get-Date -Format "dd/MM hh:mm")"
+write-host "Bonjour $env:USERNAME"
+write-host "Vous etes connecte sur la machine $env:COMPUTERNAME"
+
+read-host
